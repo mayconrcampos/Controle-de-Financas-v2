d33275829152bc0971f19652d147cbad4fce2b9f
mayconrcampos <maycon.campos@gmail.com>
2021-10-05 12:46:05 -0300

    classe DBtipogasto

--- App/Model/DBcontrole.php
+++ App/Model/DBcontrole.php
@@ -13,12 +13,68 @@ class DBcontrole {
     public function insert(Controle $controle){
         $query = "INSERT INTO controle (descricao, valor, data, categoria, comentario, tipo, iduser) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-        
+        $stmt = \App\Model\Conexao::getConn()->prepare($query);
+        $stmt->bindValue(1, $controle->getDescricao());
+        $stmt->bindValue(2, $controle->getValor());
+        $stmt->bindValue(3, $controle->getData());
+        $stmt->bindValue(4, $controle->getCategoria());
+        $stmt->bindValue(5, $controle->getComentario());
+        $stmt->bindValue(6, $controle->getTipo());
+        $stmt->bindValue(7, $controle->getIdUser());
+
+        $stmt->execute();
     }
 
     // select
 
+    public function select($id = null){
+        if(!empty($id)){
+            $query = "SELECT * FROM controle WHERE id=?";
+
+            $stmt = \App\Model\Conexao::getConn()->prepare($query);
+            $stmt->bindValue(1, $id);
+            $stmt->execute();
+        }else{
+            $query = "SELECT * FROM controle ORDER BY id ASC";
+
+            $stmt = \App\Model\Conexao::getConn()->prepare($query);
+            $stmt->execute();
+        }
+
+        if($stmt->rowCount() > 0){
+            $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            return $resultado;
+        }else{
+            return [];
+        }
+    }
+
     // update
 
+    public function update(Controle $controle){
+        $query = "UPDATE controle SET descricao=?, valor=?, data=?, categoria=?, comentario=?, tipo=? WHERE id=?";
+
+        $stmt = \App\Model\Conexao::getConn()->prepare($query);
+        $stmt->bindValue(1, $controle->getDescricao());
+        $stmt->bindValue(2, $controle->getValor());
+        $stmt->bindValue(3, $controle->getData());
+        $stmt->bindValue(4, $controle->getCategoria());
+        $stmt->bindValue(5, $controle->getComentario());
+        $stmt->bindValue(6, $controle->getTipo());
+        $stmt->bindValue(7, $controle->getID());
+        
+        $stmt->execute();
+
+    }
+
     // delete
+
+    public function delete($id){
+        $query = "DELETE FROM controle WHERE id=?";
+
+        $stmt = \App\Model\Conexao::getConn()->prepare($query);
+        $stmt->bindValue(1, $id);
+
+        $stmt->execute();
+    }
 }
--- App/Model/DBtipoGasto.php
+++ App/Model/DBtipoGasto.php
@@ -4,16 +4,62 @@ namespace App\Model;
 use App\Model\Conexao;
 use App\Model\Usuario;
 include_once("conexao.php");
-include_once("./usuarios.php");
+include_once("./TipoGasto.php");
 
 
 class DBtipoGasto {
 
     // insert
+    
+    public function insert(TipoGasto $tipo){
+        $query = "INSERT INTO TipoGasto (categoria, tipo, iduser) VALUES (?, ?, ?)";
+
+        $stmt = \App\Model\Conexao::getConn()->prepare($query);
+        $stmt->bindValue(1, $tipo->getCategoria());
+        $stmt->bindValue(2, $tipo->getTipo());
+        $stmt->bindValue(3, $tipo->getIdUser());
+
+        $stmt->execute();
+    }
 
     // select
 
+    public function select($id = null){
+        if(!empty($id)){
+            $query = "SELECT * FROM TipoGasto WHERE id=? ORDER BY id ASC";
+
+            $stmt = \App\Model\Conexao::getConn()->prepare($query);
+            $stmt->bindValue(1, $id);
+
+            $stmt->execute();
+        }else{
+            $query = "SELECT * FROM TipoGasto ORDER BY id ASC";
+
+            $stmt = \App\Model\Conexao::getConn()->prepare($query);
+            $stmt->execute();
+        }
+
+        if($stmt->rowCount() > 0){
+            $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            return $resultado;
+        }else{
+            return [];
+        }
+    }
+
     // update
+
+    public function update(TipoGasto $tipo){
+        $query = "UPDATE TipoGasto SET categoria=?, tipo=?, iduser=? WHERE id=?";
+
+        $stmt = \App\Model\Conexao::getConn()->prepare($query);
+        $stmt->bindValue(1, $tipo->getCategoria());
+        $stmt->bindValue(2, $tipo->getTipo());
+        $stmt->bindValue(3, $tipo->getIdUser());
+        $stmt->bindValue(4, $tipo->getID());
+
+        $stmt->execute();
+    }
 
     // delete
 }
